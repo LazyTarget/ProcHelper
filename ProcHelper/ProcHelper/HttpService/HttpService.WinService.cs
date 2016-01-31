ea4056bacd2b42a760ed00316ce0ed92a6289782
LazyTarget <peter.aslund@hotmail.com>
2016-01-31 16:46:55 +0100

    HttpService.WinService.cs

---
+++ ProcHelper/ProcHelper/HttpService/HttpService.WinService.cs
@@ -0,0 +1,39 @@
+﻿namespace ProcHelper
+{
+    public partial class HttpService
+    {
+        public WinServicesResponse Any(GetWinServicesRequest request)
+        {
+            var response = _worker.GetWinServices(request);
+            return response;
+        }
+
+
+        public StartWinServiceResponse Any(StartWinServiceRequest request)
+        {
+            var response = _worker.StartWinService(request);
+            return response;
+        }
+
+
+        public PauseWinServiceResponse Any(PauseWinServiceRequest request)
+        {
+            var response = _worker.PauseWinService(request);
+            return response;
+        }
+
+
+        public ContinueWinServiceResponse Any(ContinueWinServiceRequest request)
+        {
+            var response = _worker.ContinueWinService(request);
+            return response;
+        }
+
+
+        public StopWinServiceResponse Any(StopWinServiceRequest request)
+        {
+            var response = _worker.StopWinService(request);
+            return response;
+        }
+    }
+}
