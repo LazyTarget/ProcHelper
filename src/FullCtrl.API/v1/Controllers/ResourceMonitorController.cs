7a7763ae90801b84c240b8335370f39c55852280
LazyTarget <peter.aslund@hotmail.com>
2016-03-05 17:09:59 +0100

    Resource monitoring

---
+++ src/FullCtrl.API/v1/Controllers/ResourceMonitorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using FullCtrl.Base;
+
+namespace FullCtrl.API.v1.Controllers
+{
+    public class ResourceMonitorController : BaseController
+    {
+        private readonly ResourceMonitor _resourceMonitor;
+
+        public ResourceMonitorController()
+        {
+            _resourceMonitor = new ResourceMonitor();
+        }
+        
+
+        [HttpGet, HttpPost, HttpPut]
+        [Route("api/v1/resource/cpu/usage")]
+        public IResponseBase<object> GetCurrentCpuUsage()
+        {
+            object result = _resourceMonitor.GetCurrentCpuUsage();
+
+            var response = CreateResponse(result);
+            return response;
+        }
+
+    }
+}
---
+++ src/FullCtrl/Workers/ResourceMonitor.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace FullCtrl
+{
+    public class ResourceMonitor
+    {
+        private PerformanceCounter _totalCpu;
+
+        public object GetCurrentCpuUsage()
+        {
+            if (_totalCpu == null)
+            {
+                _totalCpu = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+                var f1 = _totalCpu.NextValue();
+                Thread.Sleep(1000);
+            }
+            var f2 = _totalCpu.NextValue();
+            return f2;
+        }
+    }
+}
