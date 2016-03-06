b58e2f8495bdaef94a33037b6b5f4139e1bba045
LazyTarget <peter.aslund@hotmail.com>
2016-03-06 23:51:40 +0100

    EventHub

--- src/FullCtrl.Web/Hubs/ChatHub.cs
+++ src/FullCtrl.Web/Hubs/ChatHub.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
 
 namespace FullCtrl.Web.Hubs
 {
     public class ChatHub : Hub
     {
+        public ChatHub()
+        {
+            
+        }
+
         public void Hello()
         {
             Clients.All.hello();
@@ -13,6 +22,21 @@ namespace FullCtrl.Web.Hubs
         public void Send(string name, string message)
         {
             Clients.All.addNewMessageToPage(name, message);
+
+            if (name == "Time")
+            {
+                while (true)
+                {
+                    object data = DateTime.Now.ToString();
+                    string json = JsonConvert.SerializeObject(data);
+                    EventHub.Instance?.Send("Time", "GetTime", json);
+
+                    Thread.Sleep(500);
+                }
+            }
+
         }
+
+        
     }
 }
---
+++ src/FullCtrl.Web/Hubs/EventHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace FullCtrl.Web.Hubs
+{
+    public class EventHub : Hub
+    {
+        public static EventHub Instance;
+
+        public EventHub()
+        {
+            Instance = this;
+        }
+
+
+        public void Send(string channelName, string eventName, string json)
+        {
+            Clients.All.onEvent(channelName, eventName, json);
+        }
+    }
+}
