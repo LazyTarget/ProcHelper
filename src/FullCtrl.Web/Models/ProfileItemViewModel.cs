f20864f1e8bff6e5bffc90b7fb50809f53d010d4
LazyTarget <peter.aslund@hotmail.com>
2016-03-09 08:44:10 +0100

    Menu side bar

---
+++ src/FullCtrl.Web/Models/ProfileItemViewModel.cs
@@ -0,0 +1,9 @@
+namespace FullCtrl.Web.Models
+{
+    public class ProfileItemViewModel
+    {
+        public string Name { get; set; }
+        public string Href { get; set; }
+        public string ImgSrc { get; set; }
+    }
+}
---
+++ src/FullCtrl.Web/Models/SideBarModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FullCtrl.Web.Models
+{
+    public class SideBarViewModel
+    {
+        public ProfileItemViewModel Profile { get; set; }
+        public IList<SideBarGroup> Groups { get; set; }
+    }
+
+    public class SideBarGroup
+    {
+        public IList<SideBarItem> Items { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class SideBarItem
+    {
+        public string Text { get; set; }
+        public string Href { get; set; }
+        public string ImgSrc { get; set; }
+        public bool Disabled { get; set; }
+        public bool Hidden { get; set; }
+    }
+}
