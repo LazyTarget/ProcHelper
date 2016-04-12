03b8428fa830fb4e79834d58edf9870384a54ec8
LazyTarget <peter.aslund@hotmail.com>
2016-04-12 09:10:54 +0200

    Test script: Mute and Pause script

--- src/Remotus.Plugins/Remotus.Plugins.Scripting/Data/MemoryScriptDataStore.cs
+++ src/Remotus.Plugins/Remotus.Plugins.Scripting/Data/MemoryScriptDataStore.cs
@@ -14,14 +14,14 @@ namespace Remotus.Plugins.Scripting
             var testScript = new Script
             {
                 ID = "3FE0E633-771F-4B75-BFB7-BE520EC11FF1",
-                Name = "Test script",
+                Name = "Mute and Pause Spotify script",
                 Version = "1.0.0.0",
                 Tasks = new ScriptTaskBase[]
                 {
                     new ExecuteFunctionScriptTask
                     {
-                        PluginID = "ABA6417A-65A2-4761-9B01-AA9DFFC074C0",
-                        FunctionID = "78324B37-0F93-40C2-AC56-5B1D714CFC41",
+                        PluginID = "ABA6417A-65A2-4761-9B01-AA9DFFC074C0",      // Sound
+                        FunctionID = "78324B37-0F93-40C2-AC56-5B1D714CFC41",    // Toggle mute device
                         Arguments = new FunctionArguments
                         {
                             Parameters = new ParameterCollection
@@ -38,6 +38,14 @@ namespace Remotus.Plugins.Scripting
                                 },
                             },
                         },
+                    },
+
+                    new ExecuteFunctionScriptTask
+                    {
+                        PluginID = "79A54741-590C-464D-B1E9-0CC606771493",      // Spotify
+                        //FunctionID = "2E23C7FD-BABE-4E29-A30F-C20ACB35D572",  // Toggle playing
+                        FunctionID = "B525CAD9-27E6-4709-B29B-96A32438C7D0",    // Pause
+                        Arguments = null,
                     },
                 },
             };
