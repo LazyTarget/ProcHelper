75d07723ff9137f85be2747a5a47ce42242de72d
LazyTarget <peter.aslund@hotmail.com>
2016-04-14 20:35:20 +0200

    Begun with file associations

---
+++ src/Remotus.Launcher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Remotus.Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Trace.WriteLine($"Remotus launcher!!! " +
+                                               $"UserInteractive: {Environment.UserInteractive}. " +
+                                               $"Command line: {Environment.CommandLine} " +
+                                               $"Args: {String.Join(" ", args)}");
+
+            // todo: install file associations, pointing to this exe (Assembly.GetExecutingAssembly().Location)
+            // todo: open .remotus files
+        }
+
+        private static void InstallAssociations()
+        {
+            //BrendanGrant.Helpers.FileAssociation.
+        }
+    }
+}
